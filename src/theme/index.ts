17ba1d1824612432e50a421b5affabcbcf1d5b12
guiisbarbosa <gui.silva93@hotmail.com>
2025-10-14 19:52:12 -0300

    fix: correção de nome da cor neutralLight e correção da cor neutral

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,12 +1,12 @@
 export const theme = {
   colors: {
-    c400: "#c464ec",
-    c600: "#921db7",
-    c800: "#4f0966",
-    c900: "#2e053d",
     c950: "#1c0126",
-    neutral: "#9aa1ad",
+    c800: "#4f0966",
+    c600: "#921db7",
+    c400: "#c464ec",
     highlight: "#facc15",
+    neutral: "#6e6a77",
     neutralDark: "#302937",
+    neutralLight: "#d1d5db",
   },
 };
